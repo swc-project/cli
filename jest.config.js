93705a9b0ac062d63e70b3503cfde54781e5a5ae
Grady Kuhnline <heygrady@gmail.com>
2021-08-04 22:41:44 -0700

    Use ts-jest for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
+const enableCoverage = process.env.JEST_COVERAGE === 'true'
 
-module.exports = {
-  roots: ["src"],
-  transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
-  },
-};
+module.exports = enableCoverage
+  ? {
+    // provides the most accurate coverage results
+    preset: 'ts-jest',
+    roots: ["src"],
+  }
+  : {
+    roots: ["src"],
+    // provides fastest test transforms
+    transform: {
+      '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    },
+  };

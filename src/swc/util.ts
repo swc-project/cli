4f3fdbb44c32ec039e8c13ad76d83b3b4c63a9a4
Stef <git@sril.email>
2021-10-08 14:52:06 +0100

    Don't output source maps if sourceMaps option is omitted or false

--- src/swc/util.ts
+++ src/swc/util.ts
@@ -68,13 +68,13 @@ export async function compile(
 export function outputFile(
   output: swc.Output,
   filename: string,
-  sourceMaps: undefined | swc.Options['sourceMaps']
+  sourceMaps: swc.Options['sourceMaps']
 ) {
   const destDir = dirname(filename);
   mkdirSync(destDir, { recursive: true });
 
   let code = output.code;
-  if (output.map && sourceMaps !== "inline") {
+  if (output.map && sourceMaps && sourceMaps !== "inline") {
     // we've requested for a sourcemap to be written to disk
     const fileDirName = dirname(filename);
     const mapLoc = filename + ".map";
